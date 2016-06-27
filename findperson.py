93c64d789829804ddfa02b3f60aeba6c34857a0f
yamachiyo <ymcy.1990@gmail.com>
2016-06-27 15:36:54 +0900

    Add files via upload

--- findperson.py
+++ findperson.py
@@ -12,24 +12,25 @@ class FindPerson:
 
     def get_image(self):	#get camera image
         o, self.image = self.capture.read()
-        cv2.imshow("Original", self.image)
+        #cv2.imshow("Original", self.image)
 
     def gray_image(self):	#change color gray
         self.image_g = cv2.cvtColor(self.image, cv2.COLOR_RGB2GRAY)
-        cv2.imshow("Gray", self.image_gray)
+        #cv2.imshow("Gray", self.image_g)
 
     def read_class(self):	#read classifier file
-        self.cascade = cv2.CascadeClassifier()
+        self.cascade = cv2.CascadeClassifier("/usr/share/OpenCV/haarcascades/haarcascade_fullbody.xml")
     
     def find(self):		#find person
-        person = self.cascade.detectMultiScale(self.image_g, scaleFactor=1.1, minNeighbors=2, minSize=(5,5))
+        person = self.cascade.detectMultiScale(self.image_g, scaleFactor=1.1, minNeighbors=2, minSize=(40,100))
 
         if len(person) > 0:
             for rect in person:	#make rectangle
-                cv2.rectangle(self.image, rect[0:2], (rect[0:2]+rect[2:4]), (200,0,0), thickness=2)
+                cv2.rectangle(self.image, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (200,0,0), thickness=2)
+                print rect[0:2]+(rect[2:4]-rect[0:2])/2	#center
 
         cv2.imshow("Result", self.image)
-
+        cv2.waitKey(1)
 
 
 if __name__ == "__main__":
@@ -41,4 +42,5 @@ if __name__ == "__main__":
         fp.gray_image()
         fp.find()
 
+        time.sleep(0.01)
 
--- test_findperson.py
+++ test_findperson.py
@@ -11,26 +11,28 @@ class FindPerson:
 
     def get_image(self):	#get camera image
         o, self.image = self.capture.read()
-        cv2.imshow("Original", self.image)
+        #cv2.imshow("Original", self.image)
 
     def gray_image(self):	#change color gray
         self.image_g = cv2.cvtColor(self.image, cv2.COLOR_RGB2GRAY)
-        cv2.imshow("Gray", self.image_gray)
+        #cv2.imshow("Gray", self.image_g)
 
     def read_class(self):	#read classifier file
         self.hog = cv2.HOGDescriptor()
 
     
     def find(self):		#find person
-        self.hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+        #self.hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+        self.hog.setSVMDetector(cv2.HOGDescriptor_getPeopleDetector48x96())
         hogParams = {'winStride': (8, 8), 'padding': (32, 32), 'scale': 1.05}
-        human, r = hog.detectMultiScale(self.image_g, **hogParams)
+        human, r = self.hog.detectMultiScale(self.image_g, **hogParams)
 
         for (x, y, w, h) in human:
-            cv2.rectangle(self.image, (x, y),(x+w, y+h),(0,50,255), 3)
+            cv2.rectangle(self.image, (x, y),(x+w, y+h),(200,0,0), 3)
+            print x+w/2
 
         cv2.imshow("Result", self.image)
-
+        cv2.waitKey(1)
 
 
 if __name__ == "__main__":
@@ -42,4 +44,5 @@ if __name__ == "__main__":
         fp.gray_image()
         fp.find()
 
+        time.sleep(0.01)
 
